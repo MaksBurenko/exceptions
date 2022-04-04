c6b536b8d5c9b85402f309aa95ce41011ca22737
Maks Burenko <Cherniymaks@yandex.ru>
2022-04-04 13:28:19 +0300

    123

--- src/exceptions/Exception1.java
+++ src/exceptions/Exception1.java
@@ -3,30 +3,17 @@ package exceptions;
 public class Exception1 {
 
     public static void main(String[] args) {
-
+        double x = -21.2;
+        double c = sqrt(x);
+        System.out.println(c);
     }
+
     public static double sqrt(double x) {
-        if (x<0) {
-            System.out.print("Expected non-negative number, got " + x);
-        }else {
-            double c = Math.sqrt (x);
+        if (x < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        } else {
+            double c = Math.sqrt(x);
             return c;
         }
-      return;
     }
 }
-
-/*
-public static void main(String[] args) {
-		try {
-			System.out.println(divide(20, 5));
-			System.out.println(divide(20, 0));
-		} catch (ArithmeticException ae) {
-			System.out.println(ae.getMessage());
-		}
-	}
-
-	public static double divide(int x, int y) {
-		return x / y;
-	}
- */
